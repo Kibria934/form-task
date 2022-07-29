d79bf2eab6a06f396023fe246d56aab06dbc810d
Kibria934 <kibriahossain725@gmai.com>
2022-07-29 12:18:56 +0600

    style added

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -3,29 +3,37 @@ import React from "react";
 const Form = () => {
   return (
     <div className="form-container">
-      <h2 style={{ textAlign: "left", fontSize: "18px" }}>
-        <u>Receipt Details</u>
-      </h2>
-      <div className="input-group">
-        <label htmlFor="date">Receipt Date</label>
-        <input type="" name="date" />
-      </div>
-      <div className="input-group">
-        <label htmlFor="date">Amount</label>
-        <input type="" name="date" />
-      </div>
-      <div className="input-group">
-        <label for="payment">Payment Mode</label>
-        <select name="payment">
-          <option>Cash</option>
-          <option>Credit Card</option>
-          <option>Google pay</option>
-        </select>
-      </div>
-      <div className="input-group">
-        <label htmlFor="date">Remark</label>
-        <input type="" name="date" />
-      </div>
+      <form>
+        <h2 style={{ textAlign: "left", fontSize: "18px" }}>
+          <u>Receipt Details</u>
+        </h2>
+        <div className="input-group">
+          <label htmlFor="date">Receipt Date</label>
+          <input type="" name="date" />
+        </div>
+        <div className="input-group">
+          <label htmlFor="date">Amount</label>
+          <input type="" name="date" />
+        </div>
+        <div className="input-group">
+          <label for="payment">Payment Mode</label>
+          <select name="payment">
+            <option>Cash</option>
+            <option>Credit Card</option>
+            <option>Google pay</option>
+          </select>
+        </div>
+        <div className="input-group">
+          <label htmlFor="date">Remark</label>
+          <input type="" name="date" />
+        </div>
+        <button className="btn submit" type="submit">
+          Submit
+        </button>
+        <button className="btn cancel" type="submit">
+          Cancel
+        </button>
+      </form>
     </div>
   );
 };
